31ea1532b0e1a4840a58ecddaa27a62bc410a2ef
gustavovsilva29 <gustavo.v.silva29@aluno.senai.br>
2025-11-10 13:42:12 +0000

    pytest(oficial)

--- Vida_mais/test_imc.py
+++ Vida_mais/test_imc.py
@@ -1,8 +1,36 @@
-from imc import *
+import pytest
+from imc import calcular_imc, classificar_imc
 
-def test_calcular_imc():
-    assert calcular_imc(70,1.75)
-    assert classificar_imc == 22.86
+test_calculo_imc():
+    """Teste se calcular_imc(70, 1.75) retorna 22.86"""
+    resultado = calcular_imc(70, 1.75)
+    assert resultado == 22.86
 
-def test_calcular_imc():
-    assert calcular_imc(50,0)
+def test_calculo_imc():
+    """Teste se o IMC é arredondado para 2 casas decimais"""
+    resultado = calcular_imc(80, 1.80)  # 80 / (1.8^2) = 24.691...
+    assert resultado == round(24.691358024691358, 2)
+    assert isinstance(resultado, float)
+
+def test_calculo_imc():
+    """Teste se altura = 0 gera ValueError"""
+    with pytest.raises(ValueError):
+        calcular_imc(70, 0)
+
+def test_classificacao():
+    assert classificar_imc(17.9) == "Abaixo do peso"
+
+def test_classificacao():
+    assert classificar_imc(22.0) == "Peso normal"
+
+def test_classificacao():
+    assert classificar_imc(27.3) == "Sobrepeso"
+
+def test_classificacao():
+    assert classificar_imc(33.0) == "Obesidade grau I"
+
+def test_classificacao():
+    assert classificar_imc(37.0) == "Obesidade grau II"
+
+def test_classificacao():
+    assert classificar_imc(45.0) == "Obesidade grau III"
