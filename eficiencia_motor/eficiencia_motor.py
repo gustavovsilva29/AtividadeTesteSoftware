e7641976aa32c16e4df8db6470fa5ea1284d4659
gustavovsilva29 <gustavo.v.silva29@aluno.senai.br>
2025-11-10 14:09:46 +0000

    eficiencia_motor

---
+++ eficiencia_motor/eficiencia_motor.py
@@ -0,0 +1,23 @@
+def calcular_eficiencia(potencia_saida, potencia_entrada):
+    if potencia_entrada <= 0:
+        raise ValueError("Potência de entrada deve ser maior que zero.")
+    eficiencia = (potencia_saida / potencia_entrada) *100
+    return round(eficiencia, 2)
+
+def classificar_eficiencia(eficiencia):
+    if eficiencia <80:
+        return "IE1 - Baixa Eficiência"
+    elif eficiencia <90:
+        return "IE2 - Eficiência média"
+    else:
+        return "IE3 - Alta Eficiência"
+    
+def analise_motor(potencia_saida, potencia_entrada):
+    eficiencia = calcular_eficiencia(potencia_saida, potencia_entrada)
+    classificacao = classificar_eficiencia(eficiencia)
+    return {
+        "Potencia_saida": potencia_saida,
+        "Potencia_entrada": potencia_entrada,
+        "eficiencia": eficiencia,
+        "classificacao":classificacao,
+    }
